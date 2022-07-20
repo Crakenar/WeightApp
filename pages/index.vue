b5090bfcaf4951a249f168585bd87176da91b38c
Teo Berguerre <teothou@hotmail.fr>
2022-07-20 06:24:49 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="bg-gray-300 p-8" style="border: 3px solid green">
+      <h1 class="font-bold text-gray-600 text-lg">Hello, Tailwind 3!</h1>
+      <Chart />
+    </div>
+</template>
+
+<script setup>
+import Chart from "../components/Chart"
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/server.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    Server page
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+
+</style>
